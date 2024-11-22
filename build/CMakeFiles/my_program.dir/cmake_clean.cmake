3bbae0deade31d666347acf335b94a1b7beacadf
fedoralife <tadihailukebe@gmail.com>
2024-11-22 23:54:59 +0000

    removed the web version

---
+++ build/CMakeFiles/my_program.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_program.dir/link.d"
+  "CMakeFiles/my_program.dir/src/main.c.o"
+  "CMakeFiles/my_program.dir/src/main.c.o.d"
+  "bin/my_program"
+  "bin/my_program.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/my_program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
